2841eba4eb22abf3f20f45a94eeb7300b742fce8
seyhunak <seyhunak@gmail.com>
2015-08-06 14:57:14 +0300

    first commit

--- js/script.js
+++ js/script.js
@@ -1,37 +1,79 @@
-(function($) {
-    $(function() {
-        // Focus state for append/prepend inputs
-        $('.input-prepend, .input-append').on('focus', 'input', function() {
-            $(this).closest('.control-group, form').addClass('focus');
-        }).on('blur', 'input', function() {
-            $(this).closest('.control-group, form').removeClass('focus');
+function fadedEls(el, shift) {
+  el.css('opacity', 0);
+
+  switch (shift) {
+  case undefined:
+    shift = 0
+    break;
+  case 'h':
+    shift = el.eq(0).outerHeight();
+    break;
+  case 'h/2':
+    shift = el.eq(0).outerHeight() / 2;
+    break;
+  }
+
+  $(window).resize(function() {
+    if (!el.hasClass('ani-processed')) {
+      el.eq(0).data('scrollPos', el.eq(0).offset().top - $(window).height() + shift);
+    }
+  }).scroll(function() {
+    if (!el.hasClass('ani-processed')) {
+      if ($(window).scrollTop() >= el.eq(0).data('scrollPos')) {
+        el.addClass('ani-processed');
+        el.each(function(idx) {
+          $(this).delay(idx * 200).animate({opacity:1}, 600);
         });
-       
-        (function(el) {
-            $(window).resize(function() {
-                if (!$('> img', el).hasClass('ani-processed')) {
-                    el.data('scrollPos', el.offset().top - $(window).height() + el.outerHeight() - parseInt(el.css('padding-bottom'), 10));
-                }
-            }).scroll(function() {
-                if (!$('> img', el).hasClass('ani-processed')) {
-                    if ($(window).scrollTop() >= el.data('scrollPos')) {
-                        $('> img', el).addClass('ani-processed');
-                        $('.box', el).each(function(idx) {
-                            $(this).delay(idx * 200).animate({
-                                fake : 0
-                            }, 1, 'linear', function() {
-                                $(this).addClass('ani-processed');
-                            });
-                        });
-                    }
-                }
-            });
-        })($('.content-10'));
+      }
+    }
+  });
+}
+
+
+(function($) {
+$(function() {
+
+
+  if (/msie/i.test(navigator.userAgent)) {
+    $('img').each(function() {
+      $(this).css({
+        width: $(this).attr('width') + 'px',
+        height: 'auto'
+      });
+    });
+  }
+
+
+  // Set sidebar height
+  $(window).resize(function() {
+    var h = 0;
+
+    $('body > .page-wrapper > section:not(.header-9-sub)').each(function() {
+      h += $(this).outerHeight();
     });
 
-    $(window).load(function() {
-        $('html').addClass('loaded');
-        $(window).resize().scroll();
-    });
+    $('.sidebar-content').css('height', h+'px');
+  });
+
+
+  // Faded elements
+  fadedEls($('.content-features .col-sm-7.col-sm-offset-3 img'), $('.content-features .col-sm-7.col-sm-offset-3 img').outerHeight()/3*2);
+  fadedEls($('.content-features .box [class*="fui-"]'), 'h');
+  fadedEls($('.content-9 .col-sm-3 img'), 'h');
+
+
+
+
+  $(window).resize().scroll();
+
+});
+
+
+$(window).load(function() {
+
+  $('html').addClass('loaded');
+
+  $(window).resize().scroll();
+
+});
 })(jQuery);
-
